4f0c20b49d4240883d9e4194a0ef8d00aa40f7e7
Yurii <eazylifemusic@gmail.com>
2022-08-25 18:47:02 +0600

    Реализовал отправку данных

--- js/modal.js
+++ js/modal.js
@@ -40,3 +40,77 @@
       closeModal();
     }
   });
+  // GET/PUT 
+
+  const form = document.querySelector('.modal__form');
+
+  const messages = {
+    load: "Loading",
+    success: "We have received your message. We'll reply as soon as we can.",
+    fail: "Error",
+    title: "Thank you",
+    titleFail: "Unknown error"
+  };
+
+  bindPost(form);
+
+  const postData = async (url, data) => {
+    const res = await fetch(url, {
+      method: "POST",
+      body: data,
+      headers: {
+        'Content-type': 'application/json'
+      }
+    });
+
+    return await res.json();
+  };
+
+  function bindPost(form) {
+    form.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+
+      const statusMessage = document.createElement('div');
+      statusMessage.classList.add('status');
+      statusMessage.textContent = messages.load;
+      form.append(statusMessage);
+
+      const formData = new FormData(form);
+
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
+
+      postData('http://localhost:3000/requests', json)
+        .then(data => {
+          console.log(data);
+          showThanksModal(messages.success, messages.title);
+          statusMessage.remove();
+        }).catch(() => {
+          showThanksModal(messages.fail, messages.titleFail);
+        }).finally(() => {
+          form.reset();
+        });
+    });
+  }
+
+  function showThanksModal(message, title) {
+    const firstModal = document.querySelector('.modal__dialog');
+    firstModal.style.display = 'none';
+
+    const thanksModal = document.createElement('div');
+    thanksModal.classList.add('modal__dialog');
+    thanksModal.innerHTML = `
+    <h2 class="modal__title">
+      ${title}
+    </h2>
+    <p class="modal__p">
+      ${message}
+    </p>
+    `;
+
+    const parentBlock = document.querySelector('.modal__block');
+    parentBlock.append(thanksModal);
+    setTimeout(() => {
+      thanksModal.remove();
+      firstModal.style.display = 'block';
+    }, 3000);
+  }
